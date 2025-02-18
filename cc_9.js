55cea52bfdca4ec954d4dc7b8ca749cf331a3d1f
Tuong Nguyen <yolotube21@yahoo.com>
2025-02-17 21:12:06 -0500

    Added messages to Task 1

--- cc_9.js
+++ cc_9.js
@@ -7,13 +7,12 @@ class Employee {
     };
     getDetails () {
         return `Employee: $${this.name}, ID: ${this.id}, Department: ${this.department}, Salary: $${this.salary}`
-    };
+    };// Method that returns the formatted string and get details
     calculateAnnualSalary() {
         return this.salary * 12;
-    };
+    };// Calculates the annual salary for the employee
 };
-const emp1 = new Employee("Alice Johnson", 101, "Sales", 5000);
+const emp1 = new Employee("Alice Johnson", 101, "Sales", 5000);// Instantiate employee object
 console.log(emp1.getDetails()); // Expected output: "Employee: Alice Johnson, ID: 101, Department: Sales, Salary: $5000"
-console.log(emp1.calculateAnnualSalary()); 
-// Expected output: 60000
+console.log(emp1.calculateAnnualSalary());  // Expected output: 60000
 
