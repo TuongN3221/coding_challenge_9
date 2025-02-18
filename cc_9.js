f43883a2e77f39f9ec7dc1c4be1a45199b261d56
Tuong Nguyen <yolotube21@yahoo.com>
2025-02-17 21:32:50 -0500

    Task 2 - Creating a Manager Class

--- cc_9.js
+++ cc_9.js
@@ -7,7 +7,7 @@ class Employee {
         this.salary = salary;
     };
     getDetails () {
-        return `Employee: $${this.name}, ID: ${this.id}, Department: ${this.department}, Salary: $${this.salary}`
+        return `Employee: ${this.name}, ID: ${this.id}, Department: ${this.department}, Salary: $${this.salary}`
     };// Method that returns the formatted string and get details
     calculateAnnualSalary() {
         return this.salary * 12;
@@ -17,3 +17,19 @@ const emp1 = new Employee("Alice Johnson", 101, "Sales", 5000);// Instantiate em
 console.log(emp1.getDetails()); // Expected output: "Employee: Alice Johnson, ID: 101, Department: Sales, Salary: $5000"
 console.log(emp1.calculateAnnualSalary());  // Expected output: 60000
 
+// Task 2 - Creating a Manager Class
+class Manager extends Employee {
+    constructor(name, id, department, salary, teamSize) {
+        super(name, id, department, salary)// Inherits the Employee class properties
+        this.teamSize = teamSize;// New addition that is exclusive to the Manager class
+    };
+    getDetails(){
+        return `Employee: ${this.name}, ID: ${this.id}, Department: ${this.department}, Salary: $${this.salary}, Team Size: ${this.teamSize}`;
+    };
+    calculateBonus() {
+        return this.salary * 12
+    }''
+};
+const mgr1 = new Manager("John Smith", 201, "IT", 8000, 5);
+console.log(mgr1.getDetails()); // Expected output: "Manager: John Smith, ID: 201, Department: IT, Salary: $8000, Team Size: 5"
+console.log(mgr1.calculateBonus()); // Expected output: 9600
